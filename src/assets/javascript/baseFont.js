8778ecc3be7f9d6ae73ae890ea00aab92c12e422
zzh <1023307348@qq.com>
2018-09-27 10:52:43 +0800

    change baseFont a format

--- src/assets/javascript/baseFont.js
+++ src/assets/javascript/baseFont.js
@@ -1,8 +1,8 @@
 let dpr = window.devicePixelRatio;
 let meta = document.getElementsByName("viewport")[0];
-document.addEventListener("DOMContentLoaded", (ev) => {
-    let e = ev || window.event;
-    e.stopPropagation();
-    document.getElementsByTagName("html").style.fontSize = window.innerWidth / 10;
-    meta.setAttribute("content", "width=device-width,initial-scale=" + 1 / dpr);
+document.addEventListener("DOMContentLoaded", ev => {
+  let e = ev || window.event;
+  e.stopPropagation();
+  document.getElementsByTagName("html").style.fontSize = window.innerWidth / 10;
+  meta.setAttribute("content", "width=device-width,initial-scale=" + 1 / dpr);
 });

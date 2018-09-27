dd0aa9942525cfcebda5684bbb7fc2f3646950de
zzh <1023307348@qq.com>
2018-09-27 11:14:24 +0800

    chang init end

--- src/assets/javascript/baseFont.js
+++ src/assets/javascript/baseFont.js
@@ -3,6 +3,6 @@ let meta = document.getElementsByName("viewport")[0];
 document.addEventListener("DOMContentLoaded", ev => {
   let e = ev || window.event;
   e.stopPropagation();
-  document.getElementsByTagName("html").style.fontSize = window.innerWidth / 10;
+  document.getElementsByTagName("html")[0].style.fontSize = window.innerWidth / 10 + "px";
   meta.setAttribute("content", "width=device-width,initial-scale=" + 1 / dpr);
 });
